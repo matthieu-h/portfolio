aae1c6d81191c896995ae993ee3ec599f81b078a
matthieu-h <matthieu.hatinguais@hotmail.fr>
2024-02-02 20:03:18 +0100

    suppression formulaire

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,49 +4,6 @@ const Contact = () => {
   return (
     <div id="contact" className="contact">
       <h2>Contact</h2>
-      <form
-        name="contact"
-        className="contact-form"
-        data-netlify="true"
-        onSubmit="submit"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <label htmlFor="name" title="User name" id="name-label">
-          Nom
-        </label>
-        <input
-          type="text"
-          id="name"
-          className="name"
-          placeholder="Nom"
-          name="name"
-          required
-        />
-        <label htmlFor="email" title="User email" id="email-label">
-          Email
-        </label>
-        <input
-          type="email"
-          className="email"
-          placeholder="Votre Email"
-          name="email"
-          required
-        />
-        <label htmlFor="message" title="User's message" id="msg-label">
-          Message
-        </label>
-        <textarea
-          className="msg"
-          name="message"
-          placeholder="Votre message"
-          rows="5"
-          aria-labelledby="msg-label"
-          required
-        ></textarea>
-        <button type="submit" className="contact-btn">
-          Envoyer
-        </button>
-      </form>
     </div>
   );
 };
