6ead65a1075704d5f08e25b5446dac30692cb322
matthieu-h <matthieu.hatinguais@hotmail.fr>
2024-02-02 21:15:14 +0100

    ajout mailto

--- src/components/About.js
+++ src/components/About.js
@@ -2,26 +2,26 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about" className="portfolio">
-      <h2 className="portfolio-title">À propos de moi</h2>
+    <section id="about" className="about">
+      <h2 className="about-title">À propos de moi</h2>
 
-      <p className="portfolio-para">
+      <p className="about-para">
         Après une première carrière aboutie dans le domaine du service
         après-vente de véhicules industriels en tant que responsable de service
         dans une marque internationnale de poids lourds, j'ai décidé d'un
         changement radical dans mon orientation professionnelle.
       </p>
-      <p>
+      <p className="about-para">
         En effet, il me tenait à cœur d’exercer une activité avec une part de
         création et de flexibilité tout en gardant une relation clientèle, sens
         que j’ai développé lors de mes expériences précédentes.
       </p>
-      <p>
+      <p className="about-para">
         Etant passionné d'informatique et plus particulièrement d'interface web,
         c'est donc tout naturellement que je me suis tourné vers une formation
         d'intégrateur web au sein de l'organisme OpenClassrooms.
       </p>
-      <p>
+      <p className="about-para">
         N'hésitez pas à me contacter pour une création de site web ou pour
         collaborer ensemble sur un projet web.
       </p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
     <div id="contact" className="contact">
       <h2>Contact</h2>
+      <a href="mailto:matthieu.hatinguais@hotmail.fr">
+        <button className="contact-btn">
+          <FontAwesomeIcon icon={faMessage} className="message-icon" />
+          <span className="message-icon-text">Envoyez moi un mail</span>
+        </button>
+      </a>
     </div>
   );
 };
